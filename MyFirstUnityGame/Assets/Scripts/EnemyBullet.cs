06108caab1c8f677415bd95ead0afca7c417d9e8
morinreg <76623338+morinreg@users.noreply.github.com>
2020-12-31 18:06:37 -0500

    Enemie qui nous suit et nous tire dessus

---
+++ MyFirstUnityGame/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    PlayerManager playerManager;
+    private int bulletDamage = 10;
+    private Vector3 shootDir;
+    public float moveSpeed = 1f;
+    public void Setup(Vector3 shootDirection)
+    {
+        shootDir = shootDirection;
+        Destroy(gameObject, 10f);
+    }
+
+    private void Update()
+    {
+        transform.position += shootDir * moveSpeed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+       
+        if (collision.gameObject.name == "***Player ***")
+        {
+            playerManager = PlayerManager.instance;
+            playerManager.hit(bulletDamage);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.gameObject.tag != "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ MyFirstUnityGame/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+    public int hp = 100;
+
+    public void hit(int damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0)
+        {
+            handleDeath();
+        }
+    }
+
+    public void handleDeath()
+    {
+        Debug.Log("You dead!");
+        Application.Quit();
+    }
+}
